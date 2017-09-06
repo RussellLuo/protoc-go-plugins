1cd9f902deffe3fde03f21977cc8ec0b6f5b9e44
RussellLuo <luopeng.he@gmail.com>
2017-09-06 20:38:57 +0800

    Add support for gRPC's UnaryServerInterceptor

--- protoc-gen-gohttp/generator/generator.go
+++ protoc-gen-gohttp/generator/generator.go
@@ -2,7 +2,6 @@ package generator
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/RussellLuo/protoc-go-plugins/base"
 	"github.com/golang/protobuf/proto"
@@ -37,6 +36,7 @@ import (
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
 	pb "%s"
 )`, g.Param["pb_pkg_path"]))
 }
@@ -86,27 +86,28 @@ func MakeHandler(method Method, in proto.Message) http.HandlerFunc {
 }`)
 }
 
-func (g *generator) generateService(serviceName string, methods []*google_protobuf.MethodDescriptorProto) {
+func (g *generator) generateService(fullServiceName, serviceName string, methods []*google_protobuf.MethodDescriptorProto) {
 	g.genServiceStructure(serviceName)
 	g.genServiceNewFunc(serviceName)
 	g.genServiceHandlerMapMethod(serviceName, methods)
-	g.genServiceWrapperMethods(serviceName, methods)
+	g.genServiceWrapperMethods(fullServiceName, serviceName, methods)
 }
 
 func (g *generator) genServiceStructure(serviceName string) {
 	g.P()
 	g.P("type ", serviceName, " struct {")
 	g.In()
-	g.P("srv pb.", serviceName, "Server")
+	g.P("srv         pb.", serviceName, "Server")
+	g.P("interceptor grpc.UnaryServerInterceptor")
 	g.Out()
 	g.P("}")
 }
 
 func (g *generator) genServiceNewFunc(serviceName string) {
 	g.P()
-	g.P("func New", serviceName, "(srv pb.", serviceName, "Server) *", serviceName, " {")
+	g.P("func New", serviceName, "(srv pb.", serviceName, "Server, interceptor grpc.UnaryServerInterceptor) *", serviceName, " {")
 	g.In()
-	g.P("return &", serviceName, "{srv: srv}")
+	g.P("return &", serviceName, "{srv: srv, interceptor: interceptor}")
 	g.Out()
 	g.P("}")
 }
@@ -131,15 +132,40 @@ func (g *generator) genServiceHandlerMapMethod(serviceName string, methods []*go
 	g.P("}")
 }
 
-func (g *generator) genServiceWrapperMethods(serviceName string, methods []*google_protobuf.MethodDescriptorProto) {
+func (g *generator) genServiceWrapperMethods(fullServiceName, serviceName string, methods []*google_protobuf.MethodDescriptorProto) {
 	receiverName := g.ReceiverName(serviceName)
 
 	for _, method := range methods {
-		inputTypeName := g.TypeName(method.GetInputType())
+		inputTypeName := "*pb." + g.TypeName(method.GetInputType())
+		outputTypeName := "*pb." + g.TypeName(method.GetOutputType())
+		methodName := method.GetName()
+
 		g.P()
-		g.P("func (", receiverName, " *", serviceName, ") ", method.Name, "(ctx context.Context, in proto.Message) (proto.Message, error) {")
+		g.P("func (", receiverName, " *", serviceName, ") ", methodName, "(ctx context.Context, in proto.Message) (proto.Message, error) {")
 		g.In()
-		g.P("return ", receiverName, ".srv.", method.Name, "(ctx, in.(*pb.", inputTypeName, "))")
+		g.P("if ", receiverName, ".interceptor == nil {")
+		g.In()
+		g.P("return ", receiverName, ".srv.", method.Name, "(ctx, in.(", inputTypeName, "))")
+		g.Out()
+		g.P("}")
+		g.P("out, err := ", receiverName, ".interceptor(")
+		g.In()
+		g.P("ctx,")
+		g.P("in.(", inputTypeName, "),")
+		g.P("&grpc.UnaryServerInfo{")
+		g.In()
+		g.P("Server:     ", receiverName, ".srv,")
+		g.P(`FullMethod: "`, fmt.Sprintf("/%s/%s", fullServiceName, methodName), `",`)
+		g.Out()
+		g.P("},")
+		g.P("func(ctx context.Context, req interface{}) (interface{}, error) {")
+		g.In()
+		g.P("return g.srv.SayHello(ctx, req.(", inputTypeName, "))")
+		g.Out()
+		g.P("},")
+		g.Out()
+		g.P(")")
+		g.P("return out.(", outputTypeName, "), err")
 		g.Out()
 		g.P("}")
 	}
@@ -147,45 +173,64 @@ func (g *generator) genServiceWrapperMethods(serviceName string, methods []*goog
 
 func (g *generator) generateServer(serviceNames []string) {
 	g.genServerStructure()
-	g.genServerNewFunc(serviceNames)
+	g.genServerNewFunc()
+	g.genServerRegisterMethods(serviceNames)
 	g.genServerServeMethod()
 }
 
 func (g *generator) genServerStructure() {
 	g.P(`
 type Server struct {
-	mux *http.ServeMux
+	mux         *http.ServeMux
+	interceptor grpc.UnaryServerInterceptor
 }`)
 }
 
-func (g *generator) genServerNewFunc(serviceNames []string) {
-	g.P()
-
-	args := make([]string, len(serviceNames))
-	for i, serviceName := range serviceNames {
-		args[i] = fmt.Sprintf("srv%s pb.%sServer", serviceName, serviceName)
+func (g *generator) genServerNewFunc() {
+	g.P(`
+func NewServer(interceptors ...grpc.UnaryServerInterceptor) *Server {
+	var interceptor grpc.UnaryServerInterceptor
+	switch len(interceptors) {
+	case 0:
+	case 1:
+		interceptor = interceptors[0]
+	default:
+		panic("At most one unary server interceptor can be set.")
 	}
-	g.P("func NewServer(", strings.Join(args, ", "), ") *Server {")
 
-	g.In()
-	g.P("mux := http.NewServeMux()")
+	return &Server{
+		mux:         http.NewServeMux(),
+		interceptor: interceptor,
+	}
+}`)
+}
 
+func (g *generator) genServerRegisterMethods(serviceNames []string) {
 	for _, serviceName := range serviceNames {
-		g.P("for pattern, handler := range New", serviceName, "(srv", serviceName, ").HandlerMap() {")
+		g.P()
+		g.P("func (s *Server) Register", serviceName, "Server(srv", serviceName, " pb.", serviceName, "Server) {")
 		g.In()
-		g.P("mux.Handle(pattern, handler)")
+		g.P("for pattern, handler := range New", serviceName, "(srv", serviceName, ", s.interceptor).HandlerMap() {")
+		g.In()
+		g.P("s.mux.Handle(pattern, handler)")
+		g.Out()
+		g.P("}")
 		g.Out()
 		g.P("}")
 	}
-
-	g.P("return &Server{mux: mux}")
-	g.Out()
-	g.P("}")
 }
 
 func (g *generator) genServerServeMethod() {
 	g.P(`
 func (s *Server) Serve(l net.Listener) error {
+	server := &http.Server{Handler: s.mux}
+	return server.Serve(l)
+}`)
+}
+
+func (g *generator) genServerWithUnaryInterceptor() {
+	g.P(`
+func (s *Server) WithUnaryInterceptor(i grpc.UnaryServerInterceptor) {
 	server := &http.Server{Handler: s.mux}
 	return server.Serve(l)
 }`)
@@ -197,6 +242,13 @@ func (g *generator) validateParameters() {
 	}
 }
 
+func (g *generator) getFullServiceName(packageName, originalServiceName string) string {
+	if packageName != "" {
+		return packageName + "." + originalServiceName
+	}
+	return originalServiceName
+}
+
 func (g *generator) Make(protoFile *google_protobuf.FileDescriptorProto) (*plugin.CodeGeneratorResponse_File, error) {
 	g.validateParameters()
 
@@ -206,10 +258,12 @@ func (g *generator) Make(protoFile *google_protobuf.FileDescriptorProto) (*plugi
 	g.generateMethodInterface()
 	g.generateMakeHandlerFunc()
 
+	packageName := protoFile.GetPackage()
 	serviceNames := make([]string, len(protoFile.Service))
 	for i, service := range protoFile.Service {
+		fullServiceName := g.getFullServiceName(packageName, service.GetName())
 		serviceNames[i] = gen.CamelCase(service.GetName())
-		g.generateService(serviceNames[i], service.Method)
+		g.generateService(fullServiceName, serviceNames[i], service.Method)
 	}
 
 	g.generateServer(serviceNames)
